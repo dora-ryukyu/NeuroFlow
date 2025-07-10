09eb230cbfbec11671be51bbd21d4ddffd49db7b
dora-ryukyu <hauruhaurux@gmail.com>
2025-07-10 09:39:59 +0900

    GitHub Pages用のビルドファイルを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'; // reactのインポートを追加
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+        base: "/NeuroFlow/", 
+
+        plugins: [react()], // reactプラグインを追加
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
         }
-      }
     };
 });
